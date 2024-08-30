0e830afb7e66d222a802f260b05a7640bebdf684
Flick93 <p.afflick@outlook.com>
2024-08-29 19:37:29 -0700

    First commit

---
+++ intro.py
@@ -0,0 +1,42 @@
+print("hello from python")
+# console.log("hello from python");
+
+# let variable = 1
+variable = 1
+variable = True
+variable = "h1"
+
+array = ["adrian", "Agunaga", 4, 5]
+print(array[1])
+array.append(6)
+print(array)
+array.pop()
+print(array)
+
+dict = {
+    "name":"adrian",
+    "last_name":"Aguinaga"
+
+}
+
+print(dict)
+print(dict["name"])
+
+# for(i=0; i<size; i++)
+# let temp = array[i]
+# console.log("temp")
+
+# for elements in dict:
+ #   print ("adrian")
+
+#for x in dict:
+ #   print ("adrian")
+
+for numbers in array:
+    print("adrian")
+
+#function print(){}
+def hello():
+    print("hello from python function")
+
+hello()
