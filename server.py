bdbd6bfafb6066333008d09902058345da2855e7
Flick93 <p.afflick@outlook.com>
2024-09-07 23:34:29 -0700

    third commit

--- server.py
+++ server.py
@@ -1,10 +1,10 @@
-from flask import Flask
+from flask import Flask, request
 import json
 
 app = Flask(__name__)
 
 @app.get("/")
-def home():
+def home(): 
     return "hello from flask"
 
 @app.get("/about")
@@ -12,11 +12,31 @@ def about():
     me = {"name":"adrian Aguinaga"}
     return json.dumps(me)
 
-# please create an API to footer that contains the name of the page(organika)
-
 @app.get("/footer") #Section not page
 def footer():
     pageName = {"pageName":"Organika"}
     return json.dumps(pageName)
+# please create an API to footer that contains the name of the page(organika)
+
+@app.get("/api/products")
+def products():
+    products = []
+    return  json.dumps(products)
+
+@app.post("/api/products")
+def save_products():
+    item = request.get_json()
+    print(item)
+    return json.dumps(item)
+
+@app.put("/api/products/<int:index>")
+def update_products(index):
+    update_item = request.get_json()
+    if 0<=index<len(products):
+         products[index]=update_otem
+         return json.dumps(update_item)
+    else:
+        return "That index does not exist"
+
 
 app.run(debug=True)
